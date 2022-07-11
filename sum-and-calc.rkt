6170273cdf440de87d81aef0144133a079192b02
Jimmy Lin <b00502013@gmail.com>
2022-07-11 14:27:57 +0800

    Adjusted

--- sum-and-calc.rkt
+++ sum-and-calc.rkt
@@ -1,12 +1,11 @@
 #lang racket
 
 (define sum
-  (lambda (tree)
-    (match tree
+  (lambda (exp)
+    (match exp
       ((? number? x) x)
-      (`(,tree1 ,tree2) (+ (sum tree1) (sum tree2))))))
+      (`(,e1 ,e2) (+ (sum e1) (sum e2))))))
 
-; Tests
 ; 3
 (sum '(1 2))
 ; 6
@@ -17,19 +16,17 @@
 (sum '((1 2) (3 4)))
 
 (define calc
-  (lambda (tree)
-    (match tree
+  (lambda (exp)
+    (match exp
       ((? number? x) x)
-      (`(,op ,tree1 ,tree2)
-       (let ((v1 (calc tree1))
-             (v2 (calc tree2)))
+      (`(,op ,e1 ,e2)
+       (let ((v1 (calc e1)) (v2 (calc e2)))
          (match op
            ('+ (+ v1 v2))
            ('- (- v1 v2))
            ('* (* v1 v2))
            ('/ (/ v1 v2))))))))
 
-; Tests
 ; 3
 (calc '(+ 1 2))
 ; 6
